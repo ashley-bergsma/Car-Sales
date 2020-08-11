0cbf4309d9d20c18ee0c2374e9a5a5e38f61d56a
Ashley <ashley.bergsma42@gmail.com>
2020-08-11 15:12:08 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+// react related imports 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// redux related imports
+import { createStore } from 'redux'; 
+
+// joiner imports
+import { Provider } from 'react-redux'; 
+
+import { store } from './reducers/carReducer'; 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
